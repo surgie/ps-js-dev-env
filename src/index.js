9afbb03bb2876d9bef7e03c1636f59a9f829ae4d
Travis Surgnier <surgie@gmail.com>
2017-08-20 18:13:31 -0400

    HTTP Calls and Mocking

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-import './index.css';
+import { getUsers, deleteUser } from './api/userApi';
 
-import numeral from 'numeral';
+getUsers().then(results => {
+    let usersBody = '';
 
-const courseValue = numeral(1000).format('$0,0.00');
+    results.forEach(user => {
+        usersBody += `
+            <tr>
+                <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+                <td>${user.id}</td>
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.email}</td>
+            </tr>
+        `;
+    });
 
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+    global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLinks, link => {
+        link.onclick = function (event) {
+            const el = event.target;
+            event.preventDefault();
+            deleteUser(el.attributes['data-id'].value);
+            const row = el.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
+});
