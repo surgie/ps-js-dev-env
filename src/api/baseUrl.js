f661576d51fadbbc6df4ebd0a9efb551032e1fd8
Travis Surgnier <surgie@gmail.com>
2017-08-21 15:21:22 -0400

    Add surge deployment pieces

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://stark-citadel-57639.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
